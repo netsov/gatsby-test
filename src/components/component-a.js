3564e60967c91089a3b564d8dbfa8c84484a2732
netsov <a.netsov@gmail.com>
2024-12-17 16:26:25 +0300

    lazy loading

--- src/components/component-a.js
+++ src/components/component-a.js
@@ -3,15 +3,25 @@ import * as React from "react"
 import "./component-a.css"
 import {depA} from "../utils/depa";
 
-import {Provider, defaultTheme} from '@adobe/react-spectrum';
+// import {Provider, defaultTheme} from '@adobe/react-spectrum';
+import {useEffect, useState} from "react";
+
 
 export default function ComponentA() {
-    return <Provider
-        theme={defaultTheme}
+    const [reactSpectrum, setReactSpectrum] = useState(null)
+    useEffect(() => {
+        import('@adobe/react-spectrum').then((res) => {
+            setReactSpectrum(res)
+        })
+    }, []);
+
+    return reactSpectrum ? <reactSpectrum.Provider
+        theme={reactSpectrum.defaultTheme}
     >
         <div id="test" onClick={() => {
             depA()
-        }}>ComponentA
+        }}>
+            ComponentA
         </div>
-    </Provider>
+    </reactSpectrum.Provider>: 'loading'
 }
