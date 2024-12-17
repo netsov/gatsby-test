a32488e6c93da1946458109587b69230505742ed
netsov <a.netsov@gmail.com>
2024-12-17 13:24:30 +0300

    spectrum

--- src/pages/a.js
+++ src/pages/a.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 
+import {Provider, defaultTheme} from '@adobe/react-spectrum';
+
 import "./a.css"
 // import {depA} from "../utils/depa";
 // import ComponentA from "../components/component-a";
@@ -7,18 +9,24 @@ import "./a.css"
 const ComponentA = React.lazy(() => import("../components/component-a"));
 
 
-
 const IndexPage = () => {
-  return (
-    <main  >
-      <h1>
-          APage
-      </h1>
-        {typeof window !== 'undefined' && <React.Suspense fallback={<div>Loading...</div>}>
-            <ComponentA/>
-        </React.Suspense>}
-    </main>
-  )
+    return (
+        <Provider
+            theme={defaultTheme}
+            // scale='medium'
+            // typekitId={typekitId}
+            // className={rootClassName}
+        >
+            <main>
+                <h1>
+                    APage
+                </h1>
+                {typeof window !== 'undefined' && <React.Suspense fallback={<div>Loading...</div>}>
+                    <ComponentA/>
+                </React.Suspense>}
+            </main>
+        </Provider>
+    )
 }
 
 export default IndexPage
