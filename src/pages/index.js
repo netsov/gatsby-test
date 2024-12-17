9d326c9ba6592871cbb40602b582aad983478d7c
netsov <a.netsov@gmail.com>
2024-12-17 16:58:36 +0300

    links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
 import * as React from "react"
+import {Link} from "gatsby"
 
 import "./index.css"
 
 
 const IndexPage = () => {
-  return (
-    <main  >
-      <h1>
-        index
-      </h1>
-    </main>
-  )
+    return (
+        <main>
+            <h1>
+                Index
+            </h1>
+            <div style={{display: 'flex', flexDirection: 'column', gap: 20}}>
+                {/*<Link to="/a">PageA</Link>*/}
+                {/*<Link to="/b">PageB</Link>*/}
+            </div>
+        </main>
+    )
 }
 
 export default IndexPage
